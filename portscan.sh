03a09fc5ba09a325b2172413ad8568f9ca3be665
Wesley Hop <54977819+99030063@users.noreply.github.com>
2023-02-01 16:03:12 +0100

    moved debug echo outside of loop

--- portscan.sh
+++ portscan.sh
@@ -74,10 +74,10 @@ if [ $CHOICE == 5 ]; then
         echo "test: $i"
         ipQuestion $i
         echo "Alle testen worden uitgevoerd met deze ip's: "
-        for value in "${ipARRAY[@]}"; do
+    done
+    for value in "${ipARRAY[@]}"; do
             echo "IP: $value"
         done
-    done
 elif [ $CHOICE -ge 1 ] && [ $CHOICE -le 4 ]; then
     ipQuestion "$SCAN"
     echo "single test DEBUG"
