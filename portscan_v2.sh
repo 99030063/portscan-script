ffe33941a8e1fabc3d03eade2f4ac19254fbdf62
Wesley Hop <54977819+99030063@users.noreply.github.com>
2023-02-01 14:02:06 +0100

    added check and for loop to ipquestion fucntion

--- portscan_v2.sh
+++ portscan_v2.sh
@@ -49,32 +49,34 @@ echo "DEBUG 'Choice made' $CHOICE"
 ipARRAY=()
 
 VAR=1
-for i in "${scanARRAY[@]}"
-do
-    ipQuestion(){
-        while [ $VAR -eq 1 ] ; do
-            read -p "What IP address do you want to use for the $i Portscan?" IP
-            while true ; do
-            read -p "You typed [$IP], is this correct? (y/n) " yn
-                case $yn in
-                    [yY] ) echo "ok, we will proceed";
-                        VAR=0
-                        ipARRAY[${ipARRAY[@]}]=IP
-                        break
-                        ;;
-                    [nN] ) echo "Allright, let's try again";
-                        VAR=1
-                        break
-                        ;;
-                    * ) echo "invalid response";
-                        continue;;
-                esac
-            done
+ipQuestion(){
+    while [ $VAR -eq 1 ] ; do
+        read -p "What IP address do you want to use for the $i Portscan?" IP
+        while true ; do
+        read -p "You typed [$IP], is this correct? (y/n) " yn
+            case $yn in
+                [yY] ) echo "ok, we will proceed";
+                    VAR=0
+                    ipARRAY[${ipARRAY[@]}]=IP
+                    break
+                    ;;
+                [nN] ) echo "Allright, let's try again";
+                    VAR=1
+                    break
+                    ;;
+                * ) echo "invalid response";
+                    continue;;
+            esac
         done
-    }
-done
+    done
+}
 
-ipQuestion
+if [ $CHOICE == 5 ] then
+    for i in "${scanARRAY[@]}"
+    do
+        ipQuestion
+    done
+fi
 
 # if [ $CHOICE -eq 5 ] then
 #     for i in "${scanARRAY[@]}"
