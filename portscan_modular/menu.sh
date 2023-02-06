eae05c5586983d6dc33fbb5f12c980bba14d342f
Wesley <99030063@mydavinci.nl>
2023-02-06 17:46:23 +0100

    added ipv4 address validation

--- portscan_modular/menu.sh
+++ portscan_modular/menu.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
 clear
 PS3='Maak uw keuze: '
 options=("Portscan WAN IPv4" "Portscan WAN IPv6" "Portscan WAN IPv4 and IPv6" "Portscan LAN IPv4" "Portscan LAN IPv6" "Portscan LAN IPv4 and IPv6" "Portscan WAN+LAN IPv4 + IPv6" "Rennen en wegwezen")
@@ -9,6 +26,16 @@ do
         "Portscan WAN IPv4")
             echo "Wat is het IPv4 adres?"
             read IPv4
+            valid_ip $IPv4
+            if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Welke poorten wil je scannen? (Enter = 1-65535)"
             read Ports
             ./portscan_IPv4_WAN.sh $IPv4 $Ports
@@ -25,6 +52,15 @@ do
         "Portscan WAN IPv4 and IPv6")
             echo "Wat is het IPv4 adres?"
             read IPv4
+             if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Wat is het IPv6 adres?"
             read IPv6
             echo "Welke poorten wil je scannen? (Enter = 1-65535)"
@@ -35,6 +71,15 @@ do
         "Portscan LAN IPv4")
             echo "Wat is het IPv4 adres?"
             read IPv4
+             if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Welke poorten wil je scannen? (Enter = 1-65535)"
             read Ports
             ./portscan_IPv4_LAN.sh $IPv4 $Ports
@@ -51,6 +96,15 @@ do
         "Portscan LAN IPv4 and IPv6")
             echo "Wat is het IPv4 adres?"
             read IPv4
+             if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Wat is het IPv6 adres?"
             read IPv6
             echo "Welke poorten wil je scannen? (Enter = 1-65535)"
@@ -61,10 +115,28 @@ do
         "Portscan WAN+LAN IPv4 + IPv6")
             echo "Wat is het IPv4 LAN adres?"
             read IPv4LAN
+             if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Wat is het IPv6 LAN adres?"
             read IPv6LAN
             echo "Wat is het IPv4 WAN adres?"
             read IPv4WAN
+             if valid_ip $ip; then 
+                stat='good';
+            else
+                stat='bad'; 
+            
+            fi
+            if $stat='bad';
+                break
+            fi
             echo "Wat is het IPv6 WAN adres?"
             read IPv6WAN
             echo "Welke poorten wil je scannen? (Enter = 1-65535)"
@@ -78,41 +150,3 @@ do
         *) echo "ongeldige optie $REPLY";;
     esac
 done
-
-# # ip validation function IPv4
-# function valid_ip()
-# {
-#     local  ip=$1
-#     local  stat=1
-
-#     if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-#         OIFS=$IFS
-#         IFS='.'
-#         ip=($ip)
-#         IFS=$OIFS
-#         [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-#             && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-#         stat=$?
-#     fi
-#     return $stat
-# }
-
-# # If run directly, execute some tests.
-# if [[ "$(basename $0 .sh)" == 'valid_ip' ]]; then
-#     ips='
-#         4.2.2.2
-#         a.b.c.d
-#         192.168.1.1
-#         0.0.0.0
-#         255.255.255.255
-#         255.255.255.256
-#         192.168.0.1
-#         192.168.0
-#         1234.123.123.123
-#         '
-#     for ip in $ips
-#     do
-#         if valid_ip $ip; then stat='good'; else stat='bad'; fi
-#         printf "%-20s: %s\n" "$ip" "$stat"
-#     done
-# fi
