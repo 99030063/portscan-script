80f94081dc592a5bc5e071db02841b6ebab9f22f
Wesley <99030063@mydavinci.nl>
2023-02-06 16:05:41 +0100

    changed name

--- portscan modular/portscan_IPV6_WAN.sh
+++ portscan modular/portscan_IPV6_WAN.sh
@@ -25,3 +25,4 @@ echo -e "De ipv6 UDP WAN Portscan start nu voor het IPv6 adres $2 op de poorten 
 nmap -6 -sUV -p $PORTS -P0 $CMIP6 -T4 --version-intensity=2 --min-rate=1000  | tee -a $FILENAME
 echo -e "\n---------------------------------------------------------------------------------\n" | tee -a $FILENAME
 echo -e "De portscans zijn afgerond, de resultaten staan in de map /var/roald/$DIRNAME"
+
